919afbdd7f9a1abd187934d0fa6c0fba94c16a31
Reza <rezavazifedost@gmail.com>
2025-05-16 16:45:25 +0330

    edit frontend

--- backend/config/configDB.js
+++ backend/config/configDB.js
@@ -1,8 +1,9 @@
 const mongoose = require('mongoose')
+require('dotenv').config()
 
 const connectDB = async ()=>{
     try{
-        await mongoose.connect('mongodb://localhost:27017/farmaaDB')
+        await mongoose.connect(process.env.MONGODB_URL || 'mongodb://farmaa-mongodb:27017/farmaaDB')
         console.log('connecting to database is done.');
     }catch(error){
         console.log('Error connecting to database!!', error);
